e11a95b416422a0e8739e2aee0dc34e623519737
imheretotakeyourthings <101297540+imheretotakeyourthings@users.noreply.github.com>
2022-05-25 12:26:52 +1200

    Create text bit.py

---
+++ text bit.py
@@ -0,0 +1,24 @@
+def text_bits ():
+    
+    
+    print()
+    
+    var_text = input("enter some text: ")
+    
+    
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+
+
+    print()
+    print("\'{}\' has {} characters".format(var_text, var_length))
+    print("# of bits is {} x 8".format(var_length))
+    print("We need {} nits to represent {}".format(num_bits, var_text))
+    print()
+    
+    return ""
+
+
+
+text_bits()
+
